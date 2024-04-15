d03ab7f3ee9b939175c7dd3a6a5ae83b15420192
Daniellaurin <daniellaurin21@proton.me>
2024-04-15 10:37:59 -0400

    First Commit

---
+++ init.lua
@@ -0,0 +1,5 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+vim.opt.guifont = "Fira Code Semibold:h12"
+-- vim.g.neovide_transparency = 0.80
---
+++ plugins/neorg.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- We'd like this plugin to load first out of the rest
+    config = true, -- This automatically runs `require("luarocks-nvim").setup()`
+  },
+  {
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    -- put any other flags you wanted to pass to lazy here!
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.summary"] = { -- Generates index pages
+            config = {
+              strategy = "default",
+            },
+          },
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {
+            config = {
+              icon_preset = "diamond",
+              folds = true,
+            },
+          }, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                work = "~/notes/work",
+                home = "~/notes/home",
+                school = "~/notes/school",
+              },
+              default_workspace = "school",
+              autodetect = true,
+              autodir = true,
+            },
+          },
+        },
+      })
+    end,
+  },
+}
