13e9fc736aa67a410d73e3df63a3ee3b07c9ad13
Daniel Laurin <daniellaurin21@proton.me>
2024-05-07 20:23:15 -0400

    modified my init.lua file to include colour window resizing

--- init.lua
+++ init.lua
@@ -2,4 +2,22 @@
 require("config.lazy")
 
 vim.opt.guifont = "Fira Code Semibold:h12"
--- vim.g.neovide_transparency = 0.80
+
+local modified = false
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+  callback = function()
+    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+    if normal.bg then
+      io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+      modified = true
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("UILeave", {
+  callback = function()
+    if modified then
+      io.write("\027]111\027\\")
+    end
+  end,
+})
